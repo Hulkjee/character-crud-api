ee1b60c0cb14608c5687b3ff4a923a82f4583750
Abdullah Khan <abdullahk6262@gmail.com>
2026-07-06 17:30:34 -0400

    spring boot

---
+++ src/main/java/com/csc340/character_crud_api/CharacterCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.character_crud_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterCrudApiApplication.class, args);
+	}
+
+}
